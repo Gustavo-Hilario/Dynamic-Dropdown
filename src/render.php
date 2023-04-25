759eadc731185bc11cd91fc5202e20d9cf7b0375
Gustavo_H <gustavo.hilario@automattic.com>
2023-04-24 21:38:14 -0500

    Fixing selected option

--- src/render.php
+++ src/render.php
@@ -1,11 +1,15 @@
 <?php
 $initial = $attributes['initialImage'];
+$links = array("/wp-content/uploads/2014/01/spectacles.gif" => 1, "/wp-content/uploads/2014/01/dsc20050315_145007_132.jpg" => 2);
 ?>
 
 <div>
     <select class="dynamic-dropdown">
-        <option value="/wp-content/uploads/2014/01/spectacles.gif" label="option1">Option 1</option>
-        <option value="/wp-content/uploads/2014/01/dsc20050315_145007_132.jpg" label="option2">Option 2</option>
+        <?php foreach ($links as $link => $i) {
+            $selected = $initial == $link ? 'selected' : '';
+            echo "<option value='$link' label='option$i' $selected>Option $i</option>";
+        }
+        ?>
     </select>
     <img id="renderedImage" src="<?php echo $initial ?>" />
 </div>
